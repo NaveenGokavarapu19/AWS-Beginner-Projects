0619b76522a75a4f007c837b49fc2c6f46768412
naveen gokavarapu <naveen.gokavarapu@infor.com>
2026-02-03 21:25:35 +0530

    Executed creation of IAM roles using cli. observed strange thing same command worked one time, did not work other time

--- aws_polly/main.py
+++ aws_polly/main.py
@@ -1,8 +1,8 @@
 from utils import aws_boto3_client
 
-def main():
+def lambda_handler(event, context):
     try:
-        polly_client = aws_boto3_client.return_client('polly',region_name='us-east-1',profile_name='superadmin')
+        polly_client = aws_boto3_client.return_client('polly',region_name='us-east-1')
         response = polly_client.describe_voices(
             Engine='standard',
             LanguageCode='en-IN'
@@ -10,5 +10,3 @@ def main():
         print(response)
     except Exception as e:
         print(f"Error: {e=}")
-
-main()
